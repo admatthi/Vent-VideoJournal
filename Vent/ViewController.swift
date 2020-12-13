76576503ee191d19604ed2e5d239190d813ce21f
alek <alek.matthiessen@gmail.com>
2020-12-13 12:46:19 -0500

    Initial Commit

---
+++ Vent/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Vent
+//
+//  Created by Alek Matthiessen on 12/13/20.
+//  Copyright © 2020 Alek Matthiessen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ VentTests/VentTests.swift
@@ -0,0 +1,34 @@
+//
+//  VentTests.swift
+//  VentTests
+//
+//  Created by Alek Matthiessen on 12/13/20.
+//  Copyright © 2020 Alek Matthiessen. All rights reserved.
+//
+
+import XCTest
+@testable import Vent
+
+class VentTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
